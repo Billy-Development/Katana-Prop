29699c40111b7ba377a3601e50e2a3ad92c0ea90
Bunny5578 <103382613+Bunny5578@users.noreply.github.com>
2022-08-09 21:39:06 -0400

    Add files via upload

---
+++ KatanaProp/README.lua
@@ -0,0 +1,15 @@
+this is a prop made by discord-bunny🐇#1920/https://github.com/Bunny5578
+
+to use this you must download 
+devyn-backitems - https://github.com/darktrovx/devyn-backitems
+katana weapon - https://www.gta5-mods.com/weapons/katana-sword-by-blackbradhz-the-addon-version-sandra-linson
+["weapon_katana"] = {
+    model="KatanaProp",
+    back_bone = 24818,
+    x =  0.005,
+    y = -0.14,
+    z = -0.00,
+    x_rotation = 38.0,
+    y_rotation = -180.0,
+    z_rotation = -90.0,
+    },
